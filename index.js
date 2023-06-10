ac2f7235ed6dbac3d8ba132d3535696bce4cc9b4
kipkirui63 <kiprutonyachire@gmail.com>
2023-06-10 15:09:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names) {
+  const thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful gift!`);
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Export the functions to make them accessible from other files
+module.exports = {
+  writeCards,
+  countDown,
+};
