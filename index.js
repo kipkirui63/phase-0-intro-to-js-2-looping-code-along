fc9832b65918f01f3c0786165e2a4fe68716422f
kipkirui63 <kiprutonyachire@gmail.com>
2023-06-12 15:35:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -2,20 +2,37 @@
 function writeCards(names) {
   const thankYouMessages = [];
   for (let i = 0; i < names.length; i++) {
-    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful gift!`);
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
   }
   return thankYouMessages;
 }
 
-function countDown(number) {
-  while (number >= 0) {
-    console.log(number);
-    number--;
-  }
-}
+writeCards(names);
 
-// Export the functions to make them accessible from other files
-module.exports = {
-  writeCards,
-  countDown,
-};
+function countDown(number) {
+  let i= number;
+  while (i >= 0) {
+    console.log(i); i--;
+  }
+  return number;
+}
+countDown(10)
+
+// // Export the functions to make them accessible from other files
+// module.exports = {
+//   writeCards,
+//   countDown,
+// };
+
+// const gifts = ["teddy bear","drone","doll"];
+
+// function writeGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+//   return gifts;
+// }
+
+// writeGifts(gifts);
